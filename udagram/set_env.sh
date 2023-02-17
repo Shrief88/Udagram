2d0f656d08fd1005a14afd10e5c63689f9fed218
Shrief Essam <shriefessam1999@gmail.com>
2023-02-17 21:07:53 +0200

    make sure that the app is running locally

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=Shrief
+export POSTGRES_PASSWORD=Sh00000000
+export POSTGRES_HOST=database-1.cbmsjcr1p0pd.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
