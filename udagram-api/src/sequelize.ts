dc81894e4da88db2c8e361961a687ae4bbc4625f
Shrief Essam <shriefessam1999@gmail.com>
2023-02-19 20:26:14 +0200

    update

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -6,7 +6,7 @@ export const sequelize = new Sequelize({
   password: config.password,
   database: config.database,
   host: config.host,
-
+  port: config.dbPort as unknown as number,
   dialect: "postgres",
   storage: ":memory:",
 });
