b013e12dff675b3772b205d665ce31889e25999c
Shrief Essam <shriefessam1999@gmail.com>
2023-02-19 21:16:31 +0200

    uu

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -13,6 +13,7 @@ export const config = {
   dbPort: process.env.POSTGRES_PORT,
   port: Number(process.env.PORT),
   host: process.env.POSTGRES_HOST,
+  dialect:"postgres",
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
